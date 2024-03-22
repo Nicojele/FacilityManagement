638acbea1264e6d1f9c1996a1e2e621e6983493d
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2024-03-22 10:21:27 +0100

    Adjust configs

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,md,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        '5minds-orange': '#f7a823',
+        '5minds-orange-light': '#f9b745',
+      },
+    },
+  },
+  darkMode: 'class',
+};
