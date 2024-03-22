472ccf0f14991e7dab07c6941c42b4908b5ef778
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2024-03-22 10:24:06 +0100

    Add mainpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 'use client'
 
 import styles from './page.module.css'
-import { useEffect, useState } from 'react'
-import TaskComponent from './components/taskComponent'
-import CreateTaskForm from './components/startProcessForm'
-import { getProcessInstanzess, navigateToDetailView } from './components/startsprocess'
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
-import CustomButtonComponent from './components/customButtonComponent'
+import CustomButtonComponent from './components/customButtonComponent';
+import { useEffect, useState } from 'react';
+import { getProcessInstanzess } from './components/startsprocess';
+import LogoutButton from './logoutbutton';
 
 interface TaskToolState {
   tasks: any[]
@@ -21,8 +20,7 @@ export default function Home() {
     isLoading: true
   });
 
-
-  useEffect(() => {
+   useEffect(() => {
     async function fetchData() {
       const instanzess = []
       const processInstanzes = getProcessInstanzess();
@@ -38,137 +36,33 @@ export default function Home() {
     fetchData();
   }, [])
 
-  const colDef = [
+  const columnDefs = [
     {
-      field: "description",
-      flex: 2,
-      headerClass: 'centered-header', // Specify the class for centering
-    },
-    {
-      field: "category",
-      flex: 2,
-      headerClass: 'centered-header', // Specify the class for centering
-    },
-    {
-      field: "button",
-      headerClass: 'centered-header', // Specify the class for centering
-      cellRenderer: CustomButtonComponent.bind(this)
+      headerName: 'Task',
+      children: [
+            { field: 'description', flex: 2 },
+            { field: 'category', flex: 2 },
+            { field: 'options', cellRenderer: CustomButtonComponent.bind(this), flex: 1 }
+      ],
     },
   ]
 
-  const wichtigeDringendeTaks: Array<any> = [];
-  const wichtigeTaks: Array<any> = [];
-  const dringendeTasks: Array<any> = [];
-  const unwichtigeTasks: Array<any> = [];
-
-  state.tasks.forEach((task) => {
-    if (task.category == "Wichtig & Dringend") {
-      wichtigeDringendeTaks.push(task);
-    }
-    if (task.category == "Wichtig") {
-      wichtigeTaks.push(task);
-    }
-    if (task.category == "Dringend") {
-      dringendeTasks.push(task);
-    }
-    if (task.category == "Nicht Wichtig & Nicht Dringend") {
-      unwichtigeTasks.push(task);
-    }
-  })
-
-  if (state.isLoading) return ( 
+  return ( 
     <main className={styles.main}>
       <div className={styles.body}>
-        <div className={styles.loader} />
+        <LogoutButton></LogoutButton>
+        <div className={styles.taskContainer}>
+          <div className="ag-theme-quartz" style={{ height: '95%', width: '100% '}}>
+                <AgGridReact
+                  rowData={state.tasks}
+                  columnDefs={columnDefs}
+                  onGridReady={params => {
+                    params.api.sizeColumnsToFit();
+                  }}
+                />
+            </div>
+        </div>
       </div>
     </main>
-  )
-  else return (
-    <main className={styles.main}>
-      <div className={styles.createTaskContainer}>
-        <CreateTaskForm />
-      </div>
-      <div className={styles.grid}>
-        <div
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2>
-          wichtig & dringend
-          </h2>
-          <div className={styles.taskContainer}>
-            <div className="ag-theme-quartz" style={{ height: '95%', width: '100%'}}>
-              <AgGridReact
-                rowData={wichtigeDringendeTaks}
-                columnDefs={colDef}
-                onGridReady={params => {
-                  params.api.sizeColumnsToFit();
-                }}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2>
-          wichtig & nicht dringend
-          </h2>
-            <div className={styles.taskContainer}>
-              <div className="ag-theme-quartz" style={{ height: '95%', width: '100%'}}>
-                <AgGridReact
-                  rowData={wichtigeTaks}
-                  columnDefs={colDef}
-                  onGridReady={params => {
-                    params.api.sizeColumnsToFit();
-                  }}
-                />
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2>
-          nicht wichtig & dringend
-          </h2>
-            <div className={styles.taskContainer}>
-              <div className="ag-theme-quartz" style={{ height: '95%', width: '100%'}}>
-                <AgGridReact
-                  rowData={dringendeTasks}
-                  columnDefs={colDef}
-                  onGridReady={params => {
-                    params.api.sizeColumnsToFit();
-                  }}
-                />
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2>
-          nicht wichtig & nicht dringend
-          </h2>
-            <div className={styles.taskContainer}>
-              <div className="ag-theme-quartz" style={{ height: '95%', width: '100%'}}>
-                <AgGridReact
-                  rowData={unwichtigeTasks}
-                  columnDefs={colDef}
-                  onGridReady={params => {
-                    params.api.sizeColumnsToFit();
-                  }}
-                />
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>   
   )
 }
