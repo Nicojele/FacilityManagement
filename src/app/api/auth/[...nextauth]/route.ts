636aefc3a74675cdd0d012224500b6cc617feded
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2024-03-27 08:43:06 +0100

    adjust routes file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,6 @@
-import { authConfigJwtCallback, authConfigSessionCallback } from '@5minds/processcube_app_sdk';
+import NextAuth from 'next-auth';
 
-import NextAuth, { AuthOptions } from 'next-auth';
-
-const authOptions: AuthOptions = {
-  session: { strategy: 'jwt' },
-  callbacks: {
-    jwt: authConfigJwtCallback,
-    session: authConfigSessionCallback,
-  },
-  providers: [
-    {
-      id: 'authority',
-      name: '5Minds Authority',
-      type: 'oauth',
-      wellKnown: `${process.env.PROCESSCUBE_AUTHORITY_URL}/.well-known/openid-configuration`,
-      authorization: {
-        params: {
-          scope: 'openid email profile engine_read engine_write',
-        },
-      },
-      idToken: true,
-      clientId: process.env.NEXTAUTH_CLIENT_ID,
-      clientSecret: process.env.NEXTAUTH_SECRET,
-      checks: 'pkce',
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        };
-      },
-    },
-  ],
-};
+import { authOptions } from './authOptions';
 
 const handler = NextAuth(authOptions);
 
