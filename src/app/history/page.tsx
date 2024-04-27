3db932c52adde182b5b097f3edaf0823c1bdf932
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2024-04-27 11:36:54 +0200

    Adjust history

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -7,7 +7,9 @@ import { getReviewFinishProcessInstanzess } from "../components/startsprocess";
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
-import CustomButtonComponent from '../components/reviewCreateTaskButtonComponent';
+
+import { Task } from "../components/task";
+import HistoryButtonComponent from "../components/historyButtonComponent";
 
 interface HistoryState {
   tasks: any[]
@@ -44,7 +46,7 @@ export default function ShowOrderView() {
         },
         {
           field: 'options',
-          cellRenderer: CustomButtonComponent.bind(this)
+          cellRenderer: HistoryButtonComponent.bind(this)
         }
       ],
     },
@@ -56,56 +58,14 @@ export default function ShowOrderView() {
       const processInstanzes = getReviewFinishProcessInstanzess();
       (await processInstanzes).processInstances.forEach((instanz) => {
         if (instanz.state == "finished") {
-          instanzess.push({
-            description: instanz.startToken.payload.description, category: instanz.startToken.payload.category, finished: true, processInstanzeId: instanz.processInstanceId
-          })
+          const task = new Task(instanz.startToken.payload.description, instanz.startToken.payload.category, instanz.processInstanceId, instanz.startToken.payload.date, true);
+          instanzess.push(task)
         }
       });
       setState({ tasks: instanzess, filteredTasks: state.filteredTasks, isFiltered: state.isFiltered, isLoading: false })
     }
     fetchData();
   }, [])
-
-  // function Dropdown() {
-  //   const [isOpen, setIsOpen] = useState(false);
-
-  //   const toggleDropdown = () => {
-  //       setIsOpen(!isOpen);
-  //   };
-
-  //   const handleItemClick = (category: string) => {
-  //     filterTasks(category)
-  //     setIsOpen(false);
-  //   };
-
-  //   return (
-  //       <div>
-  //         <Button icon="filter" className={styles.dropbtn} onClick={toggleDropdown}></Button>
-  //         {isOpen && (
-  //           <div className={styles.dropdownContent}>
-  //             <a href="#" onClick={() => setState({ filteredTasks: [], isFiltered: false, tasks: state.tasks, isLoading: state.isLoading })}>Ohne Filter</a>
-  //             <a href="#" onClick={() => handleItemClick("Wichtig & Dringend")}>wichtig & dringend</a>
-  //             <a href="#" onClick={() => handleItemClick("Wichtig")}>wichtig & nicht dringend</a>
-  //             <a href="#" onClick={() => handleItemClick("Dringend")}>nicht wichtig & dringend</a>
-  //             <a href="#" onClick={() => handleItemClick("Nicht Wichtig & Nicht Dringend")}>nicht wichtig & nicht dringend</a>
-  //         </div>
-  //         )}
-  //       </div>
-  //   )
-  // }
-
-  // function filterTasks(category: string) {
-  //   const filteredTasks: Array<any> = [];
-  //   for (let index = 0; index < state.tasks.length; index++) {
-  //     const task = state.tasks[index];
-      
-  //     if (task.category == category) {
-  //       filteredTasks.push(task);
-  //     }
-  //   }
-
-  //   setState({ filteredTasks: filteredTasks, isFiltered: true, tasks: state.tasks, isLoading: state.isLoading })
-  // }
 
   if (state.isLoading) {
     return(
@@ -134,25 +94,4 @@ export default function ShowOrderView() {
       </main>
     );
   }
-
-  // if (state.isFiltered == true) {
-  //   return (
-  //     <main className={styles.main}>
-  //       <div className={styles.body}>
-  //         <div className={styles.bar}>
-  //           <div className={styles.dropdown}>
-  //             <Dropdown/>
-  //           </div>
-  //         </div>
-  //         <div className={styles.historyContainer}>
-  //           {state.filteredTasks.map((task) => (
-  //             <div className={styles.content} key={task.id}>
-  //               <TaskComponent task={task}></TaskComponent>
-  //             </div>
-  //           ))}
-  //         </div>
-  //       </div>
-  //     </main>
-  //   );
-  // }
 }
